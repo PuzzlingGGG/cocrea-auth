2761ea5ef876cc15c91a4a082f9b66a62b1f61a1
PuzzlingGGG <138084889+PuzzlingGGG@users.noreply.github.com>
2024-03-10 19:26:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,27 +13,33 @@ def generate_code(length=64):
 
 @app.route('/generate_code', methods=['GET'])
 def generate_and_display_code():
-    code = generate_code()
-    expiration_time = time.time() + 120
-    codes[code] = expiration_time
-    return jsonify({'code': code, 'message': 'comment this code on <a href="https://cocrea.world/@PuzzlingGGG/CocreaAuth">cocrea.world/@PuzzlingGGG/CocreaAuth</a> without ANYTHING ELSE!'})
+    try:
+        code = generate_code()
+        expiration_time = time.time() + 120
+        codes[code] = expiration_time
+        return jsonify({'code': code, 'message': 'comment this code on <a href="https://cocrea.world/@PuzzlingGGG/CocreaAuth">cocrea.world/@PuzzlingGGG/CocreaAuth</a> without ANYTHING ELSE!'})
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
 
 @app.route('/verify_code', methods=['POST'])
 def verify_code():
-    input_code = request.json.get('code')
-    if input_code in codes and codes[input_code] > time.time():
-        response = requests.get('https://api.cocrea.world/creation-comments?creationId=1766942744927846401&page=1&perPage=20&sortField=id&sortType=DESC')
-        comments = response.json().get('body', {}).get('data', [])
-        for comment in comments:
-            if input_code in comment.get('content', ''):
-                return jsonify({'status': 'success', 'message': 'authentication successful', 'username': comment['commenter']['username'], 'id': comment['commenter']['id']})
-        return jsonify({'status': 'failure', 'message': 'code not found in comments'})
-    else:
-        return jsonify({'status': 'failure', 'message': 'invalid or expired code'})
+    try:
+        input_code = request.json.get('code')
+        if input_code in codes and codes[input_code] > time.time():
+            response = requests.get('https://api.cocrea.world/creation-comments?creationId=1766942744927846401&page=1&perPage=20&sortField=id&sortType=DESC')
+            comments = response.json().get('body', {}).get('data', [])
+            for comment in comments:
+                if input_code in comment.get('content', ''):
+                    return jsonify({'status': 'success', 'message': 'authentication successful', 'username': comment['commenter']['username'], 'id': comment['commenter']['id']})
+            return jsonify({'status': 'failure', 'message': 'code not found in comments'})
+        else:
+            return jsonify({'status': 'failure', 'message': 'invalid or expired code'})
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
